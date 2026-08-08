353e225566d42097f855843add3c8c81914edb1f
Jonathan-Aguilera <jonathanaguileratapia@gmail.com>
2026-08-08 15:55:51 -0300

    Instalación de firebase y archivos .env

--- backend/src/config/firebase.ts
+++
@@ -1,7 +0,0 @@
-// Placeholder para la conexión con Firebase Admin
-import admin from 'firebase-admin';
-
-declare const console: { log(message?: any, ...optionalParams: any[]): void };
-
-// Aquí luego inicializaremos con las credenciales
-console.log('Firebase config cargado');
